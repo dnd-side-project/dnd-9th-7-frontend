1f04fe1c8b488de17f2c393a6fba370e2709df1f
shubug1015 <shubug@naver.com>
2023-07-28 21:03:11 +0900

    Initialize project using CRA + ESLint & Prettier & Recoil 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const HomeContainer = styled.div`
+  color: tomato;
+`;
+
+function Home() {
+  return <HomeContainer>Home</HomeContainer>;
+}
+
+export default Home;
