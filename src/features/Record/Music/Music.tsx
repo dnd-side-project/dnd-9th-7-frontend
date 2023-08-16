a14b2778bf99f649c22371e9742855e6f20ffec2
SeieunYoo <isho48239@gmail.com>
2023-08-16 23:31:56 +0900

    #18 ♻️ refactor : useResizedWidth 훅 없이 css 로 비율 맞추기

--- src/features/Record/Music/Music.tsx
+++ src/features/Record/Music/Music.tsx
@@ -1,33 +1,26 @@
 import styled from '@emotion/styled';
 import { StyledTitle, StyledPlusButton } from '@features/Record/record.style';
 import { theme } from '@styles/theme';
-import { useResizeWidth } from '@common/hooks';
-import { useEffect, useState } from 'react';
 
 export const Music = () => {
-  const [width, setWidth] = useState<number>();
-  const resizedWidth = useResizeWidth();
-  useEffect(() => {
-    // NOTE : 처음 렌더링 됐을 때 main 태그가 잡히지 useEffect의 의존성 배열로 resizedWidth 를 넣어줘서 해결
-    setWidth(document.querySelector('main')?.offsetWidth);
-  }, [resizedWidth]);
   return (
     <>
       <StyledTitle>Music*</StyledTitle>
-      <StyledCD height={width as number}>
+      <StyledCD>
         <StyledPlusButton />
       </StyledCD>
     </>
   );
 };
 
-const StyledCD = styled.div<{ height: number }>`
+const StyledCD = styled.div`
   border-radius: 50rem;
   justify-content: center;
   align-items: center;
   display: flex;
   width: 100%;
-  height: ${(props) => `${(props.height - 48) * 0.1}rem`};
+  height: auto;
+  aspect-ratio: 1/1;
   border: 1px solid ${theme.color.gray08};
   position: relative;
   background-size: 'cover'; // 이미지를 div에 맞게 조절
