5051c454aa588f497be7b19e240edc12c3a599b8
SeieunYoo <isho48239@gmail.com>
2023-08-19 12:11:58 +0900

    ✨ #24 feat: emotion 배열 만들어서 emotion카테고리 선택 기능 구현

--- src/pages/situation/index.tsx
+++ src/pages/situation/index.tsx
@@ -2,55 +2,67 @@ import { SituationPageNavigation } from '@common/components/Navigation';
 import { Spacing } from '@common/components/Spacing';
 import styled from '@emotion/styled';
 import { theme } from '@styles/theme';
+import { useState } from 'react';
 
 const SituationPage = () => {
+  const [selectedEmotion, setEmotion] = useState('');
+
+  const emotion1 = [
+    { value: 'happiness', label: '행복' },
+    { value: 'excite', label: '신남' },
+    { value: 'flutter', label: '설렘' },
+    { value: 'peaceful', label: '평온' },
+  ];
+  const emotion2 = [
+    { value: 'empty', label: '공허' },
+    { value: 'depress', label: '우울' },
+    { value: 'sadness', label: '슬픔' },
+    { value: 'angry', label: '분노' },
+  ];
+  console.log(selectedEmotion);
   return (
     <>
       <SituationPageNavigation />
       <Spacing size={1.6} />
       <StyledTitle>Feeling</StyledTitle>
       <Spacing size={3} />
-      <div
-        style={{ display: 'flex', flexDirection: 'column', gap: '1.2rem', position: 'relative' }}
-      >
+      <StyledContainer>
         <StyledLine />
         <StyledLine />
         <StyledLine />
-        <div
-          style={{
-            display: 'flex',
-            gap: '3.7rem',
-            position: 'absolute',
-            transform: 'translateY(-30%)',
-          }}
-        >
-          <StyledEmoji />
-          <StyledEmoji />
-          <StyledEmoji />
-          <StyledEmoji />
-        </div>
-      </div>
+        <StyledEmojiContainer>
+          {emotion1.map((emotion) => (
+            <StyledEmojiWrapper
+              onClick={() => setEmotion(emotion.value)}
+              isSelected={selectedEmotion === emotion.value}
+            >
+              <StyledEmoji isSelected={selectedEmotion === emotion.value}>
+                {emotion.value}
+              </StyledEmoji>
+              {emotion.label}
+            </StyledEmojiWrapper>
+          ))}
+        </StyledEmojiContainer>
+      </StyledContainer>
       <Spacing size={8} />
-      <div
-        style={{ display: 'flex', flexDirection: 'column', gap: '1.2rem', position: 'relative' }}
-      >
+      <StyledContainer>
         <StyledLine />
         <StyledLine />
         <StyledLine />
-        <div
-          style={{
-            display: 'flex',
-            gap: '3.7rem',
-            position: 'absolute',
-            transform: 'translateY(-30%)',
-          }}
-        >
-          <StyledEmoji />
-          <StyledEmoji />
-          <StyledEmoji />
-          <StyledEmoji />
-        </div>
-      </div>
+        <StyledEmojiContainer>
+          {emotion2.map((emotion) => (
+            <StyledEmojiWrapper
+              onClick={() => setEmotion(emotion.value)}
+              isSelected={selectedEmotion === emotion.value}
+            >
+              <StyledEmoji isSelected={selectedEmotion === emotion.value}>
+                {emotion.value}
+              </StyledEmoji>
+              {emotion.label}
+            </StyledEmojiWrapper>
+          ))}
+        </StyledEmojiContainer>
+      </StyledContainer>
     </>
   );
 };
@@ -70,13 +82,35 @@ export const StyledLine = styled.div`
   background: ${theme.color.gray12};
 `;
 
-export const StyledEmoji = styled.div`
+export const StyledEmoji = styled.div<{ isSelected: boolean }>`
   display: flex;
   width: 5.8rem;
   height: 5.8rem;
   justify-content: center;
   align-items: center;
   border-radius: 50rem;
-  background: ${theme.color.gray12};
+  background: ${(props) => (props.isSelected ? theme.color.white : theme.color.gray12)};
   aspect-ratio: 1/1;
+`;
+
+const StyledEmojiWrapper = styled.div<{ isSelected: boolean }>`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.6rem;
+  ${theme.font.medium16}
+  color: ${(props) => (props.isSelected ? theme.color.white : theme.color.gray07)}
+`;
+
+const StyledEmojiContainer = styled.div`
+  display: flex;
+  gap: 3.7rem;
+  position: absolute;
+  transform: translateY(-30%);
+`;
+const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.2rem;
+  position: relative;
 `;
