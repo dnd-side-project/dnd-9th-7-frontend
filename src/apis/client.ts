9a334f44a5c39a4a6e5c5bc2f361a3077e15757f
SeieunYoo <isho48239@gmail.com>
2023-08-21 03:40:46 +0900

    📦 chore : bearer token 추가해야 한다는 주석 추가

--- src/apis/client.ts
+++ src/apis/client.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
-const apiUrl = process.env.REACT_APP_API_URL; // 환경 변수 값 가져오기
+const apiUrl = process.env.REACT_APP_API_URL;
 
 export const client = axios.create({
   baseURL: apiUrl,
+  // TODO: Bearer token 넣는 로직 필요
 });
