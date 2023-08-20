1430e36d9900682676e9f459720070e267839fbb
SeieunYoo <isho48239@gmail.com>
2023-08-21 03:39:55 +0900

    ✨ feat : 영상 검색 네비게이션 추가 -chore: 음악 검색 네비게이션 수정

--- src/common/components/Navigation/index.ts
+++ src/common/components/Navigation/index.ts
@@ -2,3 +2,4 @@ export * from './Navigation';
 export * from './CalenderPageNavigation';
 export * from './RecordPageNavigation';
 export * from './MusicSearchPageNavigation';
+export * from './PlayingSearchPageNavigation';
