967ee1270b071e663b48d40d84a76b19296528db
SeieunYoo <isho48239@gmail.com>
2023-08-21 03:04:42 +0900

    ✨ feat : mocking api handler 추가

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -1,4 +1,5 @@
 import * as post from './post';
 import * as auth from './auth';
+import * as music from './music';
 
-export const handlers = [...Object.values(auth), ...Object.values(post)];
+export const handlers = [...Object.values(auth), ...Object.values(post), ...Object.values(music)];
---
+++ src/mocks/handlers/music/index.ts
@@ -0,0 +1,66 @@
+import { rest } from 'msw';
+
+export const getPosts = rest.get('/api/music', (req, res, ctx) =>
+  res(
+    ctx.json([
+      {
+        tracks: {
+          offset: 1,
+          total: 10,
+          items: [
+            {
+              id: 'track01',
+              name: 'hype boy',
+              album: {
+                id: 'album01',
+                name: 'SuperShy',
+                totalTracks: 6,
+                releaseDate: '2023-07',
+                releaseDatePrecision: 'MONTH',
+                images: [
+                  {
+                    url: 'https://picsum.photos/236/354',
+                    width: 500,
+                    height: 500,
+                  },
+                ],
+              },
+              artists: [
+                {
+                  id: 'artist01',
+                  name: 'NewJeans',
+                  images: [],
+                },
+              ],
+            },
+            {
+              id: 'track01',
+              name: 'SuperShy',
+              album: {
+                id: 'album01',
+                name: 'SuperShy',
+                totalTracks: 6,
+                releaseDate: '2023-07',
+                releaseDatePrecision: 'MONTH',
+                images: [
+                  {
+                    url: 'https://picsum.photos/236/354',
+                    width: 500,
+                    height: 500,
+                  },
+                ],
+              },
+              artists: [
+                {
+                  id: 'artist01',
+                  name: 'NewJeans',
+                  images: [],
+                },
+              ],
+            },
+          ],
+        },
+      },
+    ]),
+  ),
+);
