66396f9ea3f775b9ee7d99b37113ac3c6ce7094d
shubug1015 <shubug@naver.com>
2023-08-23 02:43:03 +0900

    #40 ✨ feat: Navigation 컴포넌트 작성

---
+++ src/features/MusicLounge/components/Navigation.tsx
@@ -0,0 +1,11 @@
+import { Navigation as MusicLoungePageNavigation } from '@common/components/Navigation';
+
+export const Navigation = () => {
+  return (
+    <MusicLoungePageNavigation>
+      <MusicLoungePageNavigation.Left>
+        <span>MUSIC LOUNGE</span>
+      </MusicLoungePageNavigation.Left>
+    </MusicLoungePageNavigation>
+  );
+};
