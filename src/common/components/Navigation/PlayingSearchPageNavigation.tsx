01742ff5c9418e059d4b55c087d454d40d28e7ea
SeieunYoo <isho48239@gmail.com>
2023-08-25 03:05:36 +0900

    ✨ feat : 네비게이션 버튼 들 중 라우팅 로직 필요한 거 추가

--- src/common/components/Navigation/PlayingSearchPageNavigation.tsx
+++ src/common/components/Navigation/PlayingSearchPageNavigation.tsx
@@ -1,11 +1,13 @@
 import { Navigation } from '@common/components/Navigation';
 import { SvgIcon } from '@common/components/SvgIcon';
+import { useNavigate } from 'react-router-dom';
 
 export const PlayingSearchPageNavigation = () => {
+  const navigate = useNavigate();
   return (
     <Navigation>
       <Navigation.Left>
-        <SvgIcon id='close_white' />
+        <SvgIcon id='close_white' onClick={() => navigate(-1)} />
       </Navigation.Left>
       <Navigation.Center>
         <span>영상 첨부</span>
--- src/features/ViewRecord/components/Navigation.tsx
+++ src/features/ViewRecord/components/Navigation.tsx
@@ -1,11 +1,13 @@
 import { Navigation as ViewRecordPageNavigation } from '@common/components/Navigation';
 import { SvgIcon } from '@common/components/SvgIcon';
+import { useNavigate } from 'react-router-dom';
 
 export const Navigation = () => {
+  const navigate = useNavigate();
   return (
     <ViewRecordPageNavigation>
       <ViewRecordPageNavigation.Left>
-        <SvgIcon id='leftArrow' size={24} />
+        <SvgIcon id='leftArrow' size={24} onClick={() => navigate(-1)} />
       </ViewRecordPageNavigation.Left>
       <ViewRecordPageNavigation.Center />
       <ViewRecordPageNavigation.Right>
