7356f3a5cc7d760c7da23bba4bf7c01eb17968b4
SeieunYoo <101736358+SeieunYoo@users.noreply.github.com>
2024-04-11 19:41:50 +0900

    fix: vite 로 환경 변수 변경, msw 비동기 로 변경 (#60)
    * ♻️ refactor: 누락된 수정 추가
    
    * refactor: 누락된 부분 추가
    
    * 🐛 fix: vite 환경 변수 설정, msw 설정 변경
    
    * 📦 chore: arrow function 으로 수정

--- src/apis/client.ts
+++ src/apis/client.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiUrl = process.env.REACT_APP_API_URL;
+const apiUrl = import.meta.env.VITE_APP_API_URL;
 
 export const client = axios.create({
   baseURL: apiUrl,
--- src/index.tsx
+++ src/index.tsx
@@ -2,20 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-if (process.env.REACT_APP_API_MOCKING === 'enabled') {
-  require('./mocks');
-}
+const enableMocking = async () => {
+  if (process.env.NODE_ENV !== 'development') {
+    return;
+  }
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+  if (import.meta.env.VITE_APP_API_MOCKING === 'enabled') {
+    const { worker } = await import('./mocks/browser');
+    worker.start();
+  }
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+enableMocking().then(() => {
+  const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
+});
