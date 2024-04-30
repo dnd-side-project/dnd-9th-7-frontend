c91f4945eb35105755b1589d97d6703df22f20f5
SeieunYoo <isho48239@gmail.com>
2024-04-30 13:51:52 +0900

    refactor: index.ts 에서 export 하도록 수정

--- src/features/MusicSearch/apis/useGetMusicData.ts
+++ src/features/MusicSearch/apis/useGetMusicData.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useQuery } from '@tanstack/react-query';
-import { SearchApi } from '@/apis/SeacrhApi';
+import { SearchApi } from '@/apis';
 
 export const useGetMusicData = (value: string) => {
   const { data } = useQuery({
