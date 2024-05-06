10cb34bd334a94aec8ca737c26a65848153d2fd6
SeieunYoo <isho48239@gmail.com>
2024-05-06 11:52:45 +0900

    feat: modal 컴포넌트 구현

--- src/common/components/Overlay/Overlay.tsx
+++ src/common/components/Overlay/Overlay.tsx
@@ -2,6 +2,7 @@
 import type { PropsWithChildren } from 'react';
 
 import styled from '@emotion/styled';
+import { motion } from 'framer-motion';
 import { Portal } from '@/common/components/Portal';
 import { useClickOutside } from '@/common/hooks';
 
@@ -14,12 +15,20 @@ export const Overlay = ({ children, onClose }: PropsWithChildren<Props>) => {
 
   return (
     <Portal id='overlay'>
-      <StyledOverlayBackground ref={ref}>{children}</StyledOverlayBackground>
+      <StyledOverlayBackground
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.3 }}
+        ref={ref}
+      >
+        {children}
+      </StyledOverlayBackground>
     </Portal>
   );
 };
 
-const StyledOverlayBackground = styled.div`
+const StyledOverlayBackground = styled(motion.div)`
   position: fixed;
   inset: 0;
   z-index: 100;
