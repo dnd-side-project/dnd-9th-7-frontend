a4244003e3e1ae97a6507c4146e2521b106ee36d
Donghyun Lee <shubug@naver.com>
2024-05-13 14:07:37 +0900

    fix: git 충돌 수정

--- src/common/components/Router/Router.tsx
+++ src/common/components/Router/Router.tsx
@@ -1,13 +1,13 @@
+import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Home from '@pages/index';
-import RecordPage from '@pages/record';
-import SituationPage from '@pages/situation';
-import OnboardingPage from '@pages/onboarding';
-import MusicSearchPage from '@pages/musicSearch';
-import PlayingSearchPage from '@pages/playingSearch';
-import ViewRecordPage from '@pages/viewRecord';
-import MusicLoungePage from '@pages/musicLounge';
-import MyPlaylist from '@pages/myPlaylist';
+import Home from '@/pages/index';
+import RecordPage from '@/pages/record';
+import SituationPage from '@/pages/situation';
+import OnboardingPage from '@/pages/onboarding';
+import MusicSearchPage from '@/pages/musicSearch';
+import PlayingSearchPage from '@/pages/playingSearch';
+import ViewRecordPage from '@/pages/viewRecord';
+import MusicLoungePage from '@/pages/musicLounge';
 
 export const Router = () => {
   return (
@@ -20,7 +20,6 @@ export const Router = () => {
       <Route path='/situation' element={<SituationPage />} />
       <Route path='/view-record' element={<ViewRecordPage />} />
       <Route path='/music-lounge' element={<MusicLoungePage />} />
-      <Route path='/my-playlist' element={<MyPlaylist />} />
     </Routes>
   );
 };
