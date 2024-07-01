07379cf6c36d9d51e5d0742aad3554371eecdeec
SeieunYoo <isho48239@gmail.com>
2024-07-01 22:33:44 +0900

    feat : type module 로 바꿔서 esm 지원하는 것으로 수정

--- vite.config.ts
+++ vite.config.ts
@@ -20,4 +20,16 @@ export default defineConfig({
       '@/public': path.resolve(__dirname, './public'),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    },
+    chunkSizeWarningLimit: 500
+  }
 });
